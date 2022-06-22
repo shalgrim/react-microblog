5df9bbdd19b6a4c1629506c20bf30fd8df56ae0f
Scott Halgrim <shalgrim@gmail.com>
2022-06-22 10:34:09 -0700

    add user page; end ch 4

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import FeedPage from "./pages/FeedPage";
 import ExplorePage from "./pages/ExplorePage";
 import LoginPage from "./pages/LoginPage";
+import UserPage from "./pages/UserPage";
 
 export default function App() {
   return (
@@ -13,6 +14,7 @@ export default function App() {
           <Routes>
             <Route path="/" element={<FeedPage/>}/>
             <Route path="/explore" element={<ExplorePage/>}/>
+            <Route path="/user/:username" element={<UserPage/>}/>
             <Route path="/login" element={<LoginPage/>}/>
             <Route path="*" element={<Navigate to="/"/>}/>
           </Routes>
