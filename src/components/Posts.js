1b112036cce0fcfc85b10a5477480185130c21d6
Scott Halgrim <shalgrim@gmail.com>
2022-09-15 15:09:35 -0700

    replace fake posts with spinner render useState

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,27 +1,15 @@
+import {useState} from 'react';
+import Spinner from 'react-bootstrap/Spinner';
+
 export default function Posts() {
-  const posts = [
-    {
-      id: 1,
-      text: 'Hello, world!',
-      timestamp: 'a minute ago',
-      author: {
-        username: 'susan',
-      },
-    },
-    {
-      id: 2,
-      text: 'Second post',
-      timestamp: 'an hour ago',
-      author: {
-        username: 'john',
-      },
-    },
-  ];
+  const [posts, setPosts] = useState();
+
+  // TODO: add a side effect function to request posts here
 
   return (
       <>
-        {posts.length === 0 ?
-            <p>There are no blog posts.</p>
+        {posts === undefined ?
+            <Spinner animation="border" />
             :
             posts.map(post => {
               return (
